afea642b43ded68e0a604f6d9f023df1632fbb66
Jeremy Crantek <deucer@gmail.com>
2019-08-05 14:08:10 -0500

    Update 9.3.2.cpp

--- zybooks/9 - Pointers/9.3 Operators, new delete and ->/9.3.2.cpp
+++ zybooks/9 - Pointers/9.3 Operators, new delete and ->/9.3.2.cpp
@@ -1,3 +1,20 @@
 // 9.3.2: Allocating memory
 // Allocate memory for houseHeight using the new operator. 
 
+#include <iostream>
+using namespace std;
+
+int main() {
+   double* houseHeight = nullptr;
+
+   /* Your solution goes here  */
+   
+   houseHeight = new double;  // allocates blank memory space for a new double type named houseHeight
+
+   *houseHeight = 23;         //pointer named houseHeight set to 23
+   cout << "houseHeight is " << *houseHeight;
+
+   delete houseHeight;        // deallocates memory space
+
+   return 0;
+}
