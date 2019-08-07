3e956416270ce2b302418d1c605c53a0334df010
Jeremy Crantek <deucer@gmail.com>
2019-08-07 16:40:18 -0500

    Create 10.3.2.cpp

---
+++ zybooks/10 - Streams/10.3 Output formatting/10.3.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ios>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   double outsideTemperature = 103.45632;
+
+   /* Your solution goes here  */
+   
+   cout << setprecision(4) << outsideTemperature << endl;  // setprecision allows for decimal amount
+
+   return 0;
+}
