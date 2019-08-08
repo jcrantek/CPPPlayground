ea89ac54d2c7bdc600215c116485eded00c08b2c
Jeremy Crantek <deucer@gmail.com>
2019-08-08 12:19:32 -0500

    Create 10.4.3.cpp

---
+++ zybooks/10 - Streams/10.4 String streams/10.4.3.cpp
@@ -0,0 +1,29 @@
+// Write code that inserts userItem into the output string stream itemsOSS until the user
+// enters "Exit". Each item should be followed by a space.
+// Sample output if user input is "red purple yellow Exit":
+// red purple yellow
+
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+   string userItem;
+   ostringstream itemsOSS;
+
+   cout << "Enter items (type Exit to quit):" << endl;
+   cin >> userItem;
+
+   while (userItem != "Exit") {
+
+      /* Your solution goes here  */
+        itemsOSS << userItem << " ";
+
+      cin >> userItem;
+   }
+
+   cout << itemsOSS.str() << endl;
+
+   return 0;
+}
